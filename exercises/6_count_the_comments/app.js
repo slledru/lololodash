0b8f5f2de05cc34416e02e72bed3d3181c4b3155
slledru <slledru@msn.com>
2018-03-22 13:10:08 -0600

    using chain

--- exercises/6_count_the_comments/app.js
+++ exercises/6_count_the_comments/app.js
@@ -2,14 +2,10 @@
 var _ = require("lodash");
 
 var worker = function(data) {
-  const groups = _.groupBy(data, (entry) => entry.username)
-  const result = []
-  _.forEach(groups, (group) => {
-    const username = group[0].username
-    const count = _.size(group)
-    result.push({ username: username, comment_count: count})
-  })
-  return _.sortBy(result, (entry) => -entry.comment_count)
+  return _.chain(data)
+  .groupBy((entry) => entry.username)
+  .map((item, name) => ({username: name, comment_count: _.size(item)}))
+  .sortBy((counted) => -counted["comment_count"])
 };
 
 // export the worker function as a nodejs module
